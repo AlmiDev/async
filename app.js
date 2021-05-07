5f4aa332582dd3a5b086a300170aaa1ec0abc8ce
Mickael <almi.77.dev@gmail.com>
2021-05-07 17:07:45 +0200

    up

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
+
 console.log ('begin')
-getMember()
+
+getMember((member) => {
+    console.log(member)
+    getArticles(member, (articles) => {
+        console.log(articles)
+    })
+})
+
+//getMember()
 
 console.log('end')
 
-function getMember() {
+
+// next représentant la fonction prise en paramètre
+
+function getMember (next) {
     setTimeout( () => {
-        console.log('Member 1')
+        next('Member 1')
+    }, 4000)
+}
+
+/* gestion de l'asynchrone,
+-> via des callback
+-> des promise (promesses)
+-> async / await -> specifique ES6
+
+*/
+
+function getArticles (member, next) {
+    setTimeout (() => {
+        next([1, 2, 3])
     }, 1500)
 }
